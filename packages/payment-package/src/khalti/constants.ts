2fbf9f8b6d6ffdca0f21c6e8ef0a6898d7fef228
Bisham Kunwor <novelian.nova@gmail.com>
2024-07-08 23:40:18 +0545

    chore: Start Work on Khalti Payment Gateway

---
+++ packages/payment-package/src/khalti/constants.ts
@@ -0,0 +1,2 @@
+export const SANDBOX_URL = "https://a.khalti.com/api/v2";
+export const PRODUCTION_URL = "https://khalti.com/api/v2";
---
+++ packages/payment-package/src/khalti/index.ts
@@ -0,0 +1,9 @@
+export class KhaltiPayment {
+  constructor() {
+    console.log("test");
+  }
+
+  initiatePayment() {
+    console.log("initiating Payment");
+  }
+}
