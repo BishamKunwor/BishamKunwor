39881dfb8f392b2f1c05ac99e46cda79c9e5913e
Bisham Kunwor <novelian.nova@gmail.com>
2025-03-04 21:57:15 +0545

    feat: configure query-factory for publishing

---
+++ packages/query-factory/rollup.config.mjs
@@ -0,0 +1,22 @@
+import terser from "@rollup/plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { dts } from "rollup-plugin-dts";
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist",
+      format: "esm",
+    },
+    plugins: [nodeResolve(), typescript(), terser()],
+    external: ["@tanstack/react-query"],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+]);
