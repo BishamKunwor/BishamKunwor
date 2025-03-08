ef1cf04d0ccec5e43a2cfeac3131391f7a7816ec
Bisham Kunwor <novelian.nova@gmail.com>
2025-03-08 17:00:37 +0545

    fix: Update repo link

--- packages/query-factory/rollup.config.mjs
+++ packages/query-factory/rollup.config.mjs
@@ -18,5 +18,6 @@ export default defineConfig([
     input: "src/index.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: ["@tanstack/react-query"],
   },
 ]);
